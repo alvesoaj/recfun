7c54e49b3c2611b0aadd783a4979fcf3f7924a17
AJ Alves <aj.alves@zerokol.com>
2012-09-24 21:40:08 -0300

    first commit

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,37 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    def fatorial(num: Int): Int = {
+      if (num <= 1)
+        1
+      else
+        num * fatorial(num - 1)
+    }
+
+    fatorial(r) / (fatorial(c) * fatorial(r - c))
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
